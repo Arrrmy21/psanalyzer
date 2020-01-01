e9265913cb70ccf112339eb98dc494987e44d71d
oleksii.onyshchenko <alexonyshchenko72@gmail.com>
2020-01-01 12:13:21 +0200

    Initial commit

---
+++ src/main/java/com/onyshchenko/psanalyzer/PsanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.onyshchenko.psanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onyshchenko/psanalyzer/PsanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onyshchenko.psanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
