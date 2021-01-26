bf2fa8778373a891336c67b6705e6979b4daff3c
Army <alexonyshchenko72@gmail.com>
2021-01-26 21:15:54 +0200

    Fix query to get users id

--- src/main/java/com/onyshchenko/psanalyzer/dao/UserRepository.java
+++ src/main/java/com/onyshchenko/psanalyzer/dao/UserRepository.java
@@ -15,7 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(
             value = "select distinct users.user_id as id from users " +
                     "inner JOIN users_wish_list on users_wish_list.user_id = users.user_id " +
-                    "left outer join games on games.id = users_wish_list.wish_list " +
+                    "left outer join games on games.game_id = users_wish_list.wish_list " +
                     "left outer join prices on prices.id = games.prices_id " +
                     "where (prices.current_discount > 0)", nativeQuery = true
     )
